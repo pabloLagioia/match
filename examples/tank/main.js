f3f7fd9f208669a41faa133efc20df657590dd42
pablo <pablo.lagioia@gmail.com>
2014-05-20 20:33:44 -0300

    removed samples from test

---
+++ examples/tank/main.js
@@ -0,0 +1,69 @@
+M.registerScene("tanks", {
+
+	sprites: {
+		"grass": "grass.png",
+		"tree": "tree.gif"
+	},
+	
+	loadingScene: "loading",
+	
+	onLoad: function() {
+
+		var tileW = M.sprites.assets.grass.width,
+				tileH = M.sprites.assets.grass.height,
+				tileHW = tileH / 2,
+				tileHH = tileW / 2,
+				tilesX = 20,
+				tilesY = 20;
+			
+		var terrain = new M.Entity();
+		
+		for ( var column = 0; column < tilesX; column++ ) {
+			for ( var row = 0; row < tilesY; row++ ) {
+				var viewId = "tile" + column + ":" + row;
+				terrain.shows(viewId).as("sprite").set({
+					fill: "grass",
+					x: column * tileW + tileHW,
+					y: row * tileH + tileHH
+				})
+			}
+		}
+
+		M.add(terrain).to("world");
+
+		for ( var i = 0; i < M.random.integer(2, 3); i++ ) {
+		
+			var tree = new M.Entity();
+			
+			tree.shows("body").as("sprite").set({
+				fill: "tree",
+				x: M.random.integer(0, 640),
+				y: M.random.integer(0, 480),
+				width: 62,
+				height: 64
+			});
+			
+			tree.has("collisionGroup", 0);
+			
+			M.add(tree).to("world");
+			
+		}
+	
+		//for ( var i = 0; i < 100; i++ ) {
+			
+			tank = M.createEntity("tank");
+			tank.getAttribute("location").set(M.random.integer(0, 640), M.random.integer(0, 480));
+
+			M.add(tank).to("gameArea");
+			
+		//}
+
+	}
+
+});
+
+var tank;
+
+function main() {
+	M.setScene("tanks");
+}
